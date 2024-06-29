2a6299259acaa92214680dd0b9978249c4e466dc
william sarti <williamsartijose@hotmail.com>
2024-06-29 01:25:09 +0100

    [Testes automatizados no back end] TeTeste delete lança ResourceNotFoundException quando id não exis

--- Testes automatizados no back end/dscatalog/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
+++ Testes automatizados no back end/dscatalog/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.service;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,12 +33,19 @@ public class ProductServiceTests {
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 
     }
+    @Test
+    public void deleteShoulResourceNotFoundExceptionWhenIdDoesNotExist(){
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            service.delete(nonExistingId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
+    }
 
     @Test
     public void deleteShouldDoNothingWhenIdExists(){
         Assertions.assertDoesNotThrow(() -> {
                 service.delete(exustingId);
         });
-
+       Mockito.verify(repository, Mockito.times(1)).deleteById(exustingId);
     }
 }
