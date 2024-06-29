776950adc186fe91c85639fd25bb7187d4d8d8d2
william sarti <williamsartijose@hotmail.com>
2024-06-29 10:54:41 +0100

    [Testes automatizados no back end] Testando findAllPaged do ProductService

--- Testes automatizados no back end/dscatalog/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
+++ Testes automatizados no back end/dscatalog/src/test/java/com/devsuperior/dscatalog/service/ProductServiceTests.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscatalog.service;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.ProductService;
@@ -16,9 +17,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.mockito.Mockito.times;
 
 import java.util.List;
 import java.util.Optional;
@@ -57,6 +61,19 @@ public class ProductServiceTests {
         Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
         Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
     }
+
+    @Test
+    public void findAllPagedShouldReturnPage() {
+
+        Pageable pageable = PageRequest.of(0, 12);
+
+        Page<ProductDTO> result = service.findAllPaged(pageable);
+
+        Assertions.assertNotNull(result);
+
+        Mockito.verify(repository, times(1)).findAll(pageable);
+    }
+
     @Test
     public void deleteShoulDatabaseExceptionWhenDependentId(){
         Assertions.assertThrows(DatabaseException.class, () -> {
