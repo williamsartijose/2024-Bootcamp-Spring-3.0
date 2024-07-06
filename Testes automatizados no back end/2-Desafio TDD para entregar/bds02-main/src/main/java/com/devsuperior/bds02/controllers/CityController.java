6efd2db8597e5b122cc807411d13f190e8af4b5b
william sarti <williamsartijose@hotmail.com>
2024-07-06 17:19:53 +0100

    [Testes automatizados no back end]-[Desafio TDD para entregar] [TDD Event-City] Criando o Controllers e Service POST e DELETE

--- Testes automatizados no back end/2-Desafio TDD para entregar/bds02-main/src/main/java/com/devsuperior/bds02/controllers/CityController.java
+++ Testes automatizados no back end/2-Desafio TDD para entregar/bds02-main/src/main/java/com/devsuperior/bds02/controllers/CityController.java
@@ -5,6 +5,9 @@ import com.devsuperior.bds02.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,5 +20,19 @@ public class CityController {
     public ResponseEntity<List<CityDTO>> findAll() {
         List<CityDTO> cityDTOList = cityService.findAll();
         return ResponseEntity.ok().body(cityDTOList);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody CityDTO cityDTO) {
+        cityDTO = cityService.insert(cityDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(cityDTO.getId()).toUri();
+        return ResponseEntity.created(uri).body(cityDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
