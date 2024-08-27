28760d7e0f3ad46544cd5b8f70d8f9c911cc439d
william sarti <williamsartijose@hotmail.com>
2024-08-27 23:28:20 +0100

    [Validação e segurança] CRUD de usuários PARTE 1

--- Validação e segurança/dscatalog-bds3-backend2-main/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ Validação e segurança/dscatalog-bds3-backend2-main/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,7 +17,6 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "tb_user")
 public class User implements  Serializable {
@@ -25,11 +25,14 @@ public class User implements  Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String firstName;
+    private String lastName;
 
+    @Column(unique = true)
     private String email;
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
@@ -38,9 +41,11 @@ public class User implements  Serializable {
     public User() {
     }
 
-    public User(Long id, String email, String password) {
+    public User(Long id, String firstName, String lastName, String email, String password) {
         super();
         this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
@@ -51,6 +56,22 @@ public class User implements  Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -99,3 +120,4 @@ public class User implements  Serializable {
     }
 
 }
+
