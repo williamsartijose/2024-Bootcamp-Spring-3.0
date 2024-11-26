48484197b15a7f0958740cdeb4899e9d33354a6e
william sarti <williamsartijose@hotmail.com>
2024-11-26 17:14:42 +0000

    [Validação e segurança] DESAFIO Validação e segurança

--- 1- Desafio Bootcamp Spring 3.0/Backend/src/main/java/com/wsj/bootcampspring/services/exceptions/ClientNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wsj.bootcampspring.services.exceptions;
-
-public class ClientNotFoundException extends  RuntimeException{
-    private static final long serialVersioUID = 1L;
-    public ClientNotFoundException(String msg){
-        super(msg);
-    }
-}
